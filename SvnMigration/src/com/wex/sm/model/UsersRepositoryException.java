22ab162829baa325e45457cd57acf1618d6eb3ed
sudhakar reddy <sudhakar.reddy@wexeurope.com>
2015-04-28 10:10:21 +0000

    git-svn-id: https://WE-DSK-005.corporatepay.local/svn/Java_projects/trunk@15 de4987a2-d98c-fe41-a0b8-93f6ac7f3a18

--- SvnMigration/src/com/wex/sm/model/UsersRepositoryException.java
+++ SvnMigration/src/com/wex/sm/model/UsersRepositoryException.java
@@ -7,4 +7,12 @@ public class UsersRepositoryException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	
+    public UsersRepositoryException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public UsersRepositoryException(String msg) {
+        super(msg);
+    }
 }
